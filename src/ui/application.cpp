da17f61c9ff52c3ae32eb0989438a362c656e13f
Nick Logozzo <nlogozzo225@gmail.com>
2022-10-14 12:32:54 -0400

    V2022.10.2-beta1

--- src/ui/application.cpp
+++ src/ui/application.cpp
@@ -13,7 +13,7 @@ Application::Application(const std::string& id, GApplicationFlags flags) : m_adw
     m_appInfo.setName("Nickvision Tube Converter");
     m_appInfo.setShortName("Tube Converter");
     m_appInfo.setDescription("An easy-to-use YouTube video downloader.");
-    m_appInfo.setVersion("2022.10.2-next");
+    m_appInfo.setVersion("2022.10.2-beta1");
     m_appInfo.setChangelog("<ul><li>New application icon (Thanks @martin-desktops!)</li><li>Improved video url checking</li></ul>");
     m_appInfo.setGitHubRepo("https://github.com/nlogozzo/NickvisionTubeConverter");
     m_appInfo.setIssueTracker("https://github.com/nlogozzo/NickvisionTubeConverter/issues/new");
