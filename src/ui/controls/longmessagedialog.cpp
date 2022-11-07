6d1c7b4f85eacd8aa0a46d69f8adc4040ad30f82
Nick Logozzo <nlogozzo225@gmail.com>
2022-11-07 09:08:19 -0500

    Fix LongMessageDialog Label Wrap

--- src/ui/controls/longmessagedialog.cpp
+++ src/ui/controls/longmessagedialog.cpp
@@ -8,6 +8,8 @@ LongMessageDialog::LongMessageDialog(GtkWindow* parent, const std::string& title
     m_scrolledWindow = gtk_scrolled_window_new();
     gtk_widget_set_size_request(m_scrolledWindow, 500, 500);
     m_lblDescription = gtk_label_new(description.c_str());
+    gtk_label_set_wrap(GTK_LABEL(m_lblDescription), true);
+    gtk_label_set_justify(GTK_LABEL(m_lblDescription), GTK_JUSTIFY_CENTER);
     gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(m_scrolledWindow), m_lblDescription);
     adw_message_dialog_set_extra_child(ADW_MESSAGE_DIALOG(m_gobj), m_scrolledWindow);
 }
