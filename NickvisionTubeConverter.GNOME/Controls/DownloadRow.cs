48cf28e28e6d33b45adcae2df27281c8224c949f
DaPigGuy <mcpepig123@gmail.com>
2023-02-12 03:14:41 -0800

    GNOME - Fancier progress bars!

--- NickvisionTubeConverter.GNOME/Controls/DownloadRow.cs
+++ NickvisionTubeConverter.GNOME/Controls/DownloadRow.cs
@@ -11,17 +11,20 @@ namespace NickvisionTubeConverter.GNOME.Controls;
 /// </summary>
 public partial class DownloadRow : Adw.ActionRow
 {
+    private delegate bool GSourceFunc(nint data);
     [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial nint g_main_context_default();
-
+    private static partial uint g_idle_add(GSourceFunc function, nint data);
     [LibraryImport("libadwaita-1.so.0", StringMarshalling = StringMarshalling.Utf8)]
-    private static partial void g_main_context_iteration(nint context, [MarshalAs(UnmanagedType.I1)] bool blocking);
+    private static partial uint g_timeout_add(uint interval, GSourceFunc function, nint data);
 
+    private readonly Localizer _localizer;
     private readonly Download _download;
     private readonly Gtk.ProgressBar _progBar;
+    private readonly Gtk.Label _progLabel;
     private readonly Gtk.Image _imgStatus;
     private readonly Gtk.LevelBar _levelBar;
     private readonly Adw.ViewStack _viewStack;
+    private DownloadProgress _lastProgress;
 
     /// <summary>
     /// Constructs a DownloadRow
@@ -29,6 +32,7 @@ public partial class DownloadRow : Adw.ActionRow
     /// <param name="download">The download displayed by the row</param>
     public DownloadRow(Localizer localizer, Download download)
     {
+        _localizer = localizer;
         _download = download;
         //Row Settings
         SetTitle(download.Path);
@@ -40,11 +44,19 @@ public partial class DownloadRow : Adw.ActionRow
         AddPrefix(_imgStatus);
         //Box Downloading
         var boxDownloading = Gtk.Box.New(Gtk.Orientation.Horizontal, 6);
+        var boxProgress = Gtk.Box.New(Gtk.Orientation.Vertical, 3);
+        boxProgress.SetValign(Gtk.Align.Center);
         //Progress Bar
         _progBar = Gtk.ProgressBar.New();
         _progBar.SetValign(Gtk.Align.Center);
         _progBar.SetSizeRequest(300, -1);
-        boxDownloading.Append(_progBar);
+        boxProgress.Append(_progBar);
+        //Progress Label
+        _progLabel = Gtk.Label.New(localizer["DownloadState.Preparing"]);
+        _progLabel.SetValign(Gtk.Align.Center);
+        _progLabel.AddCssClass("caption");
+        boxProgress.Append(_progLabel);
+        boxDownloading.Append(boxProgress);
         //Stop Button
         var btnStop = Gtk.Button.New();
         btnStop.SetValign(Gtk.Align.Center);
@@ -66,6 +78,10 @@ public partial class DownloadRow : Adw.ActionRow
         _viewStack.AddNamed(boxDownloading, "downloading");
         _viewStack.AddNamed(boxDone, "done");
         AddSuffix(_viewStack);
+        g_timeout_add(30, d => {
+            _progBar.Pulse();
+            return _lastProgress == null;
+        }, 0);
     }
 
     /// <summary>
@@ -74,13 +90,34 @@ public partial class DownloadRow : Adw.ActionRow
     public async void Start()
     {
         var success = await _download.RunAsync(true, new Progress<DownloadProgress>(p => {
-            g_main_context_iteration(g_main_context_default(), false);
-            _progBar.SetFraction(p.Progress);
+            _lastProgress = p;
+            switch (p.State)
+            {
+                case DownloadState.PreProcessing:
+                case DownloadState.PostProcessing:
+                    g_timeout_add(30, d => {
+                        _progBar.Pulse();
+                        return _lastProgress.State == DownloadState.PreProcessing || _lastProgress.State == DownloadState.PostProcessing;
+                    }, 0);
+                    break;
+                case DownloadState.Downloading:
+                    g_idle_add(d => {
+                        _progBar.SetFraction(p.Progress);
+                        return false;
+                    }, 0);
+                    break;
+            }
+            g_idle_add(d => {
+                _progLabel.SetText(_localizer["DownloadState." + Enum.GetName(typeof(DownloadState), p.State)]);
+                return false;
+            }, 0);
         }));
-        _imgStatus.RemoveCssClass("accent");
-        _imgStatus.AddCssClass(success ? "success" : "error");
-        g_main_context_iteration(g_main_context_default(), false);
-        _viewStack.SetVisibleChildName("done");
-        _levelBar.SetValue(success ? 1 : 0);
+        g_idle_add(d => {
+            _imgStatus.RemoveCssClass("accent");
+            _imgStatus.AddCssClass(success ? "success" : "error");
+            _viewStack.SetVisibleChildName("done");
+            _levelBar.SetValue(success ? 1 : 0);
+            return false;
+        }, 0);
     }
 }
