06fbaddebb4161a308b0d237d99dbaf0a9aa4b9b
Nick Logozzo <nlogozzo225@gmail.com>
2023-02-11 21:54:55 -0500

    Create DependencyManager.cs

---
+++ NickvisionTubeConverter.Shared/Helpers/DependencyManager.cs
@@ -0,0 +1,84 @@
+﻿using NickvisionTubeConverter.Shared.Models;
+using System.IO;
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using YoutubeDLSharp;
+
+namespace NickvisionTubeConverter.Shared.Helpers;
+
+internal static class DependencyManager
+{
+    /// <summary>
+    /// The path for yt-dlp
+    /// </summary>
+    public static string YtdlpPath
+    {
+        get
+        {
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Current.Name}yt-dlp.exe";
+            }
+            var prefixes = new List<string>() {
+                Directory.GetParent(Directory.GetParent(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!))!.FullName)!.FullName,
+                Directory.GetParent(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!))!.FullName,
+                "/usr"
+            };
+            foreach (var prefix in prefixes)
+            {
+                var path = $"{prefix}/bin/yt-dlp";
+                if (File.Exists(path))
+                {
+                    return path;
+                }
+            }
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// The path for ffmpeg
+    /// </summary>
+    public static string Ffmpeg
+    {
+        get
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Current.Name}ffmpeg.exe";
+            }
+            var prefixes = new List<string>() {
+                Directory.GetParent(Directory.GetParent(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!))!.FullName)!.FullName,
+                Directory.GetParent(Path.GetFullPath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!))!.FullName,
+                "/usr"
+            };
+            foreach (var prefix in prefixes)
+            {
+                var path = $"{prefix}/bin/ffmpeg";
+                if (File.Exists(path))
+                {
+                    return path;
+                }
+            }
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// Downloads dependencies (For Windows ONLY)
+    /// </summary>
+    /// <returns>True if successful, else false. Also false if non-windows system</returns>
+    public static async Task<bool> DownloadDependencies()
+    {
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            return false;
+        }
+        await YoutubeDL.DownloadYtDlpBinary(YtdlpPath.Remove(YtdlpPath.IndexOf("yt-dlp.exe")));
+        await YoutubeDL.DownloadFFmpegBinary(Ffmpeg.Remove(Ffmpeg.IndexOf("ffmpeg.exe")));
+        return true;
+    }
+}
