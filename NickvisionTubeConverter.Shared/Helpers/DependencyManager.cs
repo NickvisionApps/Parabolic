57c35d5e445744e310a05dcbf1f611e5e9348179
Nick Logozzo <nlogozzo225@gmail.com>
2023-03-09 20:41:55 -0500

    Shared - Download Python 3.11.2 For WIndows With Correct OS Arch

--- NickvisionTubeConverter.Shared/Helpers/DependencyManager.cs
+++ NickvisionTubeConverter.Shared/Helpers/DependencyManager.cs
@@ -52,17 +52,23 @@ internal static class DependencyManager
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 var pythonDirPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Current.Name}{Path.DirectorySeparatorChar}Python{Path.DirectorySeparatorChar}";
-                var pythonDllPath = $"{pythonDirPath}python-3.7.3-embed-amd64{Path.DirectorySeparatorChar}python37.dll";
-                var pythonLibPath = $"{pythonDirPath}python-3.7.3-embed-amd64{Path.DirectorySeparatorChar}Lib{Path.DirectorySeparatorChar}";
-                if (!File.Exists(Ffmpeg) || Configuration.Current.WinUIFfmpegVersion != new Version(5, 3, 1))
+                var pythonType = RuntimeInformation.OSArchitecture switch
+                {
+                    Architecture.X86 => "python-3.11.2-embed-win32",
+                    Architecture.Arm => "python-3.11.2-embed-win32",
+                    Architecture.Arm64 => "python-3.11.2-embed-arm64",
+                    _ => "python-3.11.2-embed-amd64"
+                };
+                var pythonDllPath = $"{pythonDirPath}{pythonType}{Path.DirectorySeparatorChar}python311.dll";
+                var pythonLibPath = $"{pythonDirPath}{pythonType}{Path.DirectorySeparatorChar}Lib{Path.DirectorySeparatorChar}";
+                if (!File.Exists(Ffmpeg))
                 {
                     await FFmpegDownloader.GetLatestVersion(FFmpegVersion.Official, Ffmpeg.Remove(Ffmpeg.IndexOf("ffmpeg.exe")));
-                    Configuration.Current.WinUIFfmpegVersion = new Version(5, 3, 1);
-                    Configuration.Current.Save();
                 }
-                if (!File.Exists(pythonDllPath) || !Directory.Exists(pythonLibPath) || Configuration.Current.WinUIPythonVersion != new Version(3, 7, 3) || (Configuration.Current.WinUIYtdlpVersion != new Version(2023, 3, 4)))
+                if (!File.Exists(pythonDllPath) || !Directory.Exists(pythonLibPath) || Configuration.Current.WinUIPythonVersion != new Version(3, 11, 2) || (Configuration.Current.WinUIYtdlpVersion != new Version(2023, 3, 4)))
                 {
                     Python.Deployment.Installer.InstallPath = pythonDirPath;
+                    Python.Deployment.Installer.Source = new Python.Deployment.Installer.DownloadInstallationSource() { DownloadUrl = $"https://www.python.org/ftp/python/3.11.2/{pythonType}.zip" };
                     Python.Runtime.Runtime.PythonDLL = pythonDllPath;
                     if(Directory.Exists(pythonLibPath))
                     {
@@ -71,7 +77,7 @@ internal static class DependencyManager
                     await Python.Deployment.Installer.SetupPython(true);
                     await Python.Deployment.Installer.InstallWheel(typeof(MainWindowController).Assembly, "yt_dlp-any.whl", true);
                     await Python.Deployment.Installer.SetupPython();
-                    Configuration.Current.WinUIPythonVersion = new Version(3, 7, 3);
+                    Configuration.Current.WinUIPythonVersion = new Version(3, 11, 2);
                     Configuration.Current.WinUIYtdlpVersion = new Version(2023, 3, 4);
                     Configuration.Current.Save();
                 }
@@ -98,8 +104,9 @@ internal static class DependencyManager
             }
             return true;
         }
-        catch
+        catch(Exception e)
         {
+            Console.WriteLine(e);
             return false;
         }
     }
