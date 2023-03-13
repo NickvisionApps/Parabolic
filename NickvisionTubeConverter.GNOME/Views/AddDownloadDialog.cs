cb4154b3f2bbb18cdeda9ee0bfec08500cd75d3c
Nick Logozzo <nlogozzo225@gmail.com>
2023-03-13 18:20:37 -0400

    Update AddDownloadDialog.cs

--- NickvisionTubeConverter.GNOME/Views/AddDownloadDialog.cs
+++ NickvisionTubeConverter.GNOME/Views/AddDownloadDialog.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
 
 namespace NickvisionTubeConverter.GNOME.Views;
 
