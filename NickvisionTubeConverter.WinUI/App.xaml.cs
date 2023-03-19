039843fa461c7f36f618ff9e4732990b436205bd
Nick Logozzo <nlogozzo225@gmail.com>
2023-03-19 14:16:26 -0400

    Start Next Ver

--- NickvisionTubeConverter.WinUI/App.xaml.cs
+++ NickvisionTubeConverter.WinUI/App.xaml.cs
@@ -27,8 +27,8 @@ public partial class App : Application
         _mainWindowController.AppInfo.Name = "Nickvision Tube Converter";
         _mainWindowController.AppInfo.ShortName = "Tube Converter";
         _mainWindowController.AppInfo.Description = $"{_mainWindowController.Localizer["Description"]}.";
-        _mainWindowController.AppInfo.Version = "2023.3.0";
-        _mainWindowController.AppInfo.Changelog = "- Tube Converter has been rewritten in C#.\nWith the C# rewrite, Tube Converter is now available on Windows!";
+        _mainWindowController.AppInfo.Version = "2023.3.1-next";
+        _mainWindowController.AppInfo.Changelog = "- ";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/nlogozzo/NickvisionTubeConverter");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/nlogozzo/NickvisionTubeConverter/issues/new");
         _mainWindowController.AppInfo.SupportUrl = new Uri("https://github.com/nlogozzo/NickvisionTubeConverter/discussions");
