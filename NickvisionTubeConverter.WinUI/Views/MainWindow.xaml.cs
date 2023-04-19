14b6b76a854c9f85508410466f27bf2885f836b4
Nick Logozzo <nlogozzo225@gmail.com>
2023-04-18 22:31:34 -0400

    WinUI - Upgrade to WindowsAppSDK 1.3

--- NickvisionTubeConverter.WinUI/Views/MainWindow.xaml.cs
+++ NickvisionTubeConverter.WinUI/Views/MainWindow.xaml.cs
@@ -32,10 +32,7 @@ public sealed partial class MainWindow : Window
     private bool _isOpened;
     private readonly MainWindowController _controller;
     private readonly IntPtr _hwnd;
-    private readonly AppWindow _appWindow;
     private bool _isActived;
-    private readonly SystemBackdropConfiguration _backdropConfiguration;
-    private readonly MicaController? _micaController;
     private bool _closeAllowed;
     private TrayIconWithContextMenu? _taskbarIcon;
     private DispatcherTimer _taskbarTimer;
@@ -51,7 +48,6 @@ public sealed partial class MainWindow : Window
         _isOpened = false;
         _controller = controller;
         _hwnd = WindowNative.GetWindowHandle(this);
-        _appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(_hwnd));
         _isActived = true;
         _closeAllowed = false;
         _taskbarIcon = null;
@@ -60,7 +56,7 @@ public sealed partial class MainWindow : Window
             Interval = new TimeSpan(0, 0, 1)
         };
         //Register Events
-        _appWindow.Closing += Window_Closing;
+        AppWindow.Closing += Window_Closing;
         _controller.NotificationSent += NotificationSent;
         _controller.UICreateDownloadRow = CreateDownloadRow;
         _controller.UIMoveDownloadRow = MoveDownloadRow;
@@ -69,43 +65,25 @@ public sealed partial class MainWindow : Window
         _taskbarTimer.Tick += TaskbarTimer_Tick;
         //Set TitleBar
         TitleBarTitle.Text = _controller.AppInfo.ShortName;
-        _appWindow.Title = TitleBarTitle.Text;
-        _appWindow.SetIcon(@"Assets\org.nickvision.tubeconverter.ico");
+        AppWindow.Title = TitleBarTitle.Text;
+        AppWindow.SetIcon(@"Assets\org.nickvision.tubeconverter.ico");
         TitlePreview.Text = _controller.IsDevVersion ? _controller.Localizer["Preview", "WinUI"] : "";
         if (AppWindowTitleBar.IsCustomizationSupported())
         {
-            _appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
-            TitleBarLeftPaddingColumn.Width = new GridLength(_appWindow.TitleBar.LeftInset);
-            TitleBarRightPaddingColumn.Width = new GridLength(_appWindow.TitleBar.RightInset);
-            _appWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
-            _appWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+            AppWindow.TitleBar.ExtendsContentIntoTitleBar = true;
+            TitleBarLeftPaddingColumn.Width = new GridLength(AppWindow.TitleBar.LeftInset);
+            TitleBarRightPaddingColumn.Width = new GridLength(AppWindow.TitleBar.RightInset);
+            AppWindow.TitleBar.ButtonBackgroundColor = Colors.Transparent;
+            AppWindow.TitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
         }
         else
         {
             TitleBar.Visibility = Visibility.Collapsed;
             NavView.Margin = new Thickness(0, 0, 0, 0);
         }
-        //Setup Backdrop
-        WindowsSystemDispatcherQueueHelper.EnsureWindowsSystemDispatcherQueueController();
-        _backdropConfiguration = new SystemBackdropConfiguration()
-        {
-            IsInputActive = true,
-            Theme = ((FrameworkElement)Content).ActualTheme switch
-            {
-                ElementTheme.Default => SystemBackdropTheme.Default,
-                ElementTheme.Light => SystemBackdropTheme.Light,
-                ElementTheme.Dark => SystemBackdropTheme.Dark,
-                _ => SystemBackdropTheme.Default
-            }
-        };
-        if (MicaController.IsSupported())
-        {
-            _micaController = new MicaController();
-            _micaController.AddSystemBackdropTarget(this.As<ICompositionSupportsSystemBackdrop>());
-            _micaController.SetSystemBackdropConfiguration(_backdropConfiguration);
-        }
+        SystemBackdrop = new MicaBackdrop();
         //Window Sizing
-        _appWindow.Resize(new SizeInt32(800, 600));
+        AppWindow.Resize(new SizeInt32(800, 600));
         User32.ShowWindow(_hwnd, ShowWindowCommand.SW_SHOWMAXIMIZED);
         //Taskbar Icon
         ToggleTaskbarIcon(null, EventArgs.Empty);
@@ -165,9 +143,7 @@ public sealed partial class MainWindow : Window
         _isActived = e.WindowActivationState != WindowActivationState.Deactivated;
         //Update TitleBar
         TitleBarTitle.Foreground = (SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"];
-        _appWindow.TitleBar.ButtonForegroundColor = ((SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"]).Color;
-        //Update Backdrop
-        _backdropConfiguration.IsInputActive = _isActived;
+        AppWindow.TitleBar.ButtonForegroundColor = ((SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"]).Color;
     }
 
     /// <summary>
@@ -180,7 +156,7 @@ public sealed partial class MainWindow : Window
         if (_controller.RunInBackground)
         {
             e.Cancel = true;
-            _appWindow.Hide();
+            AppWindow.Hide();
         }
         else
         {
@@ -210,7 +186,6 @@ public sealed partial class MainWindow : Window
                 _taskbarTimer.Stop();
                 _controller.StopAllDownloads();
                 _controller.Dispose();
-                _micaController?.Dispose();
                 _taskbarIcon?.Dispose();
             }
         }
@@ -225,15 +200,7 @@ public sealed partial class MainWindow : Window
     {
         //Update TitleBar
         TitleBarTitle.Foreground = (SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"];
-        _appWindow.TitleBar.ButtonForegroundColor = ((SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"]).Color;
-        //Update Backdrop
-        _backdropConfiguration.Theme = sender.ActualTheme switch
-        {
-            ElementTheme.Default => SystemBackdropTheme.Default,
-            ElementTheme.Light => SystemBackdropTheme.Light,
-            ElementTheme.Dark => SystemBackdropTheme.Dark,
-            _ => SystemBackdropTheme.Default
-        };
+        AppWindow.TitleBar.ButtonForegroundColor = ((SolidColorBrush)Application.Current.Resources[_isActived ? "WindowCaptionForeground" : "WindowCaptionForegroundDisabled"]).Color;
     }
 
     /// <summary>
@@ -241,7 +208,7 @@ public sealed partial class MainWindow : Window
     /// </summary>
     /// <param name="sender">object?</param>
     /// <param name="e">EventArgs</param>
-    private void ShowWindow(object? sender, EventArgs e) => _appWindow.Show();
+    private void ShowWindow(object? sender, EventArgs e) => AppWindow.Show();
 
     /// <summary>
     /// Occurs when the TaskbarMenuQuit item is clicked
@@ -255,8 +222,7 @@ public sealed partial class MainWindow : Window
             _controller.StopAllDownloads();
             _taskbarTimer.Stop();
             _taskbarIcon!.Remove();
-            _appWindow.Hide();
-            _micaController?.Dispose();
+            AppWindow.Hide();
             _taskbarIcon!.Dispose();
             _controller.Dispose();
             Environment.Exit(0);
