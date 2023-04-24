9d484589a88b037a982f707195aa6fa64cfd39f0
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-04-24 12:24:46 +0300

    Change metadata desc and make appname translatable

--- NickvisionTubeConverter.GNOME/Program.cs
+++ NickvisionTubeConverter.GNOME/Program.cs
@@ -45,7 +45,7 @@ public partial class Program
         _mainWindowController = new MainWindowController();
         _mainWindowController.AppInfo.ID = "org.nickvision.tubeconverter";
         _mainWindowController.AppInfo.Name = "Nickvision Tube Converter";
-        _mainWindowController.AppInfo.ShortName = "Tube Converter";
+        _mainWindowController.AppInfo.ShortName = _mainWindowController.Localizer["ShortName"];
         _mainWindowController.AppInfo.Description = $"{_mainWindowController.Localizer["Description"]}.";
         _mainWindowController.AppInfo.Version = "2023.4.3-next";
         _mainWindowController.AppInfo.Changelog = "<ul><li>Fixed a random crash users where experiencing when downloading</li><li>Updated translations (Thanks everyone on Weblate!)</li></ul>";
