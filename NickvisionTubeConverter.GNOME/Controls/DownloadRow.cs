e0613a25a8c2dfd5557a3aa2e66e950e33654649
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-04-30 21:55:50 +0300

    GNOME - Use Fragments-like status icons

--- NickvisionTubeConverter.GNOME/Controls/DownloadRow.cs
+++ NickvisionTubeConverter.GNOME/Controls/DownloadRow.cs
@@ -175,7 +175,7 @@ public partial class DownloadRow : Adw.Bin, IDownloadRowControl
         {
             _progressBar.SetFraction(1.0);
             _statusIcon.RemoveCssClass("accent");
-            _statusIcon.AddCssClass("error");
+            _statusIcon.AddCssClass("stopped");
             _statusIcon.SetFromIconName("process-stop-symbolic");
             _stateViewStack.SetVisibleChildName("done");
             _levelBar.SetValue(0);
