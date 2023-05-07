kbdharun/NickvisionTubeConverter

All - Rename quality -> audio quality / Dropped `Good` quality
Audio quality currently affects video quality. This will change when
video resolutions can be selected.

NickvisionTubeConverter.GNOME/Views/AddDownloadDialog.cs
@@ -60,7 +60,7 @@ public partial class AddDownloadDialog : Adw.MessageDialog
     [Gtk.Connect] private readonly Gtk.Button _backButton;
     [Gtk.Connect] private readonly Gtk.Label _titleLabel;
     [Gtk.Connect] private readonly Adw.ComboRow _fileTypeRow;
-    [Gtk.Connect] private readonly Adw.ComboRow _qualityRow;
+    [Gtk.Connect] private readonly Adw.ComboRow _audioQualityRow;
     [Gtk.Connect] private readonly Adw.ComboRow _subtitleRow;
     [Gtk.Connect] private readonly Adw.EntryRow _saveFolderRow;
     [Gtk.Connect] private readonly Gtk.MenuButton _saveWarning;
@@ -137,7 +137,7 @@ public partial class AddDownloadDialog : Adw.MessageDialog
         _controller.Accepted = e.Response == "ok";
         if (_controller.Accepted)
         {
-            _controller.PopulateDownloads(_videoUrlInfo!, (MediaFileType)_fileTypeRow.GetSelected(), (Quality)_qualityRow.GetSelected(), (Subtitle)_subtitleRow.GetSelected(), _saveFolderRow.GetText(), _overwriteSwitch.GetActive());
+            _controller.PopulateDownloads(_videoUrlInfo!, (MediaFileType)_fileTypeRow.GetSelected(), (AudioQuality)_audioQualityRow.GetSelected(), (Subtitle)_subtitleRow.GetSelected(), _saveFolderRow.GetText(), _overwriteSwitch.GetActive());
         }
     }
 

NickvisionApps/Parabolic

NickvisionTubeConverter.GNOME/Views/AddDownloadDialog.cs
@@ -69,7 +69,7 @@ public partial class AddDownloadDialog : Adw.Window
     [Gtk.Connect] private readonly Gtk.Box _downloadPage;
     [Gtk.Connect] private readonly Gtk.Button _backButton;
     [Gtk.Connect] private readonly Gtk.DropDown _fileTypeDropDown;
-    [Gtk.Connect] private readonly Gtk.DropDown _qualityDropDown;
+    [Gtk.Connect] private readonly Gtk.DropDown _audioQualityDropDown;
     [Gtk.Connect] private readonly Gtk.DropDown _subtitlesDropDown;
     [Gtk.Connect] private readonly Adw.EntryRow _saveFolderRow;
     [Gtk.Connect] private readonly Gtk.Button _selectSaveFolderButton;
@@ -178,7 +178,7 @@ public partial class AddDownloadDialog : Adw.Window
         //Add Download Button
         _addDownloadButton.OnClicked += (sender, e) =>
         {
-            _controller.PopulateDownloads(_videoUrlInfo!, (MediaFileType)_fileTypeDropDown.GetSelected(), (Quality)_qualityDropDown.GetSelected(), (Subtitle)_subtitlesDropDown.GetSelected(), _saveFolderString, _overwriteSwitch.GetActive(), _speedLimitSwitch.GetActive());
+            _controller.PopulateDownloads(_videoUrlInfo!, (MediaFileType)_fileTypeDropDown.GetSelected(), (AudioQuality)_audioQualityDropDown.GetSelected(), (Subtitle)_subtitlesDropDown.GetSelected(), _saveFolderString, _overwriteSwitch.GetActive(), _speedLimitSwitch.GetActive());
             OnDownload?.Invoke(this, EventArgs.Empty);
         };
         _addDownloadButton.SetSensitive(false);