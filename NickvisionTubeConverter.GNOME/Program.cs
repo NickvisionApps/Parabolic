43d350cb718f67ca17f4ce531ebaafb94cd8e67d
Nick Logozzo <nlogozzo225@gmail.com>
2023-07-01 09:24:30 -0400

    V2023.7.0

--- NickvisionTubeConverter.GNOME/Program.cs
+++ NickvisionTubeConverter.GNOME/Program.cs
@@ -48,7 +48,7 @@ public partial class Program
         _mainWindowController.AppInfo.Name = "Nickvision Tube Converter";
         _mainWindowController.AppInfo.ShortName = _("Parabolic");
         _mainWindowController.AppInfo.Description = $"{_("Download web video and audio")}.";
-        _mainWindowController.AppInfo.Version = "2023.7.0-next";
+        _mainWindowController.AppInfo.Version = "2023.7.0";
         _mainWindowController.AppInfo.Changelog = "<ul><li>Fixed an issue where the Help page couldn't be accessed after the name change</li><li>Updated translations (Thanks everyone on Weblate!)</li></ul>";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/NickvisionApps/Parabolic");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/NickvisionApps/Parabolic/issues/new");
