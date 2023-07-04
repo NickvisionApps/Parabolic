0f5896f5bdeaa8cda608babe981a644f88aef4ec
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-07-04 19:00:44 +0300

    Flatpak - Change cake sources

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -11,13 +11,8 @@ set -eu
 if [ -d "nuget-sources" ]
 then
     mv flatpak/NuGet.config ./
-    mv cake-sources/cake.tool.3.0.0.nupkg flatpak/cake_repo/cake.tool/3.0.0/
-    mv cake-sources/cake.filehelpers.6.1.3.nupkg flatpak/cake_repo/cake.filehelpers/6.1.3/
-    dotnet tool restore
-    dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained --sources=nuget-sources
-else
-    dotnet tool restore
-    dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained
 fi
+dotnet tool restore
+dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained
 
 dotnet cake --target=Install
