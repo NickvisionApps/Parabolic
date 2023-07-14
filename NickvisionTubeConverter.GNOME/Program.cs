e79fb89b2136c860cf8199e390ac0bd45649d776
Nick Logozzo <nlogozzo225@gmail.com>
2023-07-15 01:06:50 +0200

    All - Start Next Ver

--- NickvisionTubeConverter.GNOME/Program.cs
+++ NickvisionTubeConverter.GNOME/Program.cs
@@ -48,8 +48,8 @@ public partial class Program
         _mainWindowController.AppInfo.Name = "Nickvision Tube Converter";
         _mainWindowController.AppInfo.ShortName = _("Parabolic");
         _mainWindowController.AppInfo.Description = $"{_("Download web video and audio")}.";
-        _mainWindowController.AppInfo.Version = "2023.7.2";
-        _mainWindowController.AppInfo.Changelog = "<ul><li>Added the ability to select a download's audio language if more than one are available</li><li>Added the option to allow for only Windows-supported characters in filenames</li><li>Updated translations (Thanks everyone on Weblate!)</li></ul>";
+        _mainWindowController.AppInfo.Version = "2023.7.3-next";
+        _mainWindowController.AppInfo.Changelog = "<ul><li>Updated translations (Thanks everyone on Weblate!)</li></ul>";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/NickvisionApps/Parabolic");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/NickvisionApps/Parabolic/issues/new");
         _mainWindowController.AppInfo.SupportUrl = new Uri("https://github.com/NickvisionApps/Parabolic/discussions");
