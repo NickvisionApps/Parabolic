d380622970a3b0d83264fa0890c6f5452aab9547
Nick Logozzo <nlogozzo225@gmail.com>
2023-07-22 00:48:49 +0200

    Shared - DownloadHistory Object

---
+++ NickvisionTubeConverter.Shared/Models/DownloadHistory.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace NickvisionTubeConverter.Shared.Models;
+
+/// <summary>
+/// A model for download history
+/// </summary>
+public class DownloadHistory
+{
+    private static readonly string HistoryPath = $"{Configuration.ConfigDir}{Path.DirectorySeparatorChar}downloadHistory.json";
+    
+    /// <summary>
+    /// The download history
+    /// </summary>
+    public List<Uri> History { get; set; }
+    
+    /// <summary>
+    /// Constructs a DownloadHistory
+    /// </summary>
+    public DownloadHistory()
+    {
+        History = new List<Uri>();
+    }
+    
+    /// <summary>
+    /// Gets a DownloadHistory object
+    /// </summary>
+    public static DownloadHistory Current
+    {
+        get
+        {
+            try
+            {
+                return JsonSerializer.Deserialize<DownloadHistory>(File.ReadAllText(HistoryPath)) ?? new DownloadHistory();
+            }
+            catch
+            {
+                return new DownloadHistory();
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Saves the DownloadHistory to disk
+    /// </summary>
+    public void Save() => File.WriteAllText(HistoryPath, JsonSerializer.Serialize(this));
+}
