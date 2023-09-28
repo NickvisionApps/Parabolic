30f94dd45d346adff2c3690d2b45b6e9ffd9b17f
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-09-28 20:15:54 +0300

    Shared - Fix audio-only stream detection

--- NickvisionTubeConverter.Shared/Models/MediaUrlInfo.cs
+++ NickvisionTubeConverter.Shared/Models/MediaUrlInfo.cs
@@ -216,7 +216,7 @@ public class MediaUrlInfo
                 _tryVideo = true;
             }
         }
-        if(VideoResolutions.Count == 0 && mediaInfo.HasKey("video_ext") && mediaInfo["video_ext"].As<string>() != "None")
+        if(VideoResolutions.Count == 0 && mediaInfo.HasKey("video_ext") && mediaInfo["video_ext"].As<string>() != "none")
         {
             VideoResolutions.Add(new VideoResolution(0, 0));
         }
