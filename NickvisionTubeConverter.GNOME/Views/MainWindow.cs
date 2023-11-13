afcf38be5e987611ace43364c135ce76d1b29920
Nick Logozzo <nlogozzo225@gmail.com>
2023-11-13 09:46:47 -0500

    GNOME - MainWindow Use ToolbarView

--- NickvisionTubeConverter.GNOME/Views/MainWindow.cs
+++ NickvisionTubeConverter.GNOME/Views/MainWindow.cs
@@ -33,7 +33,6 @@ public partial class MainWindow : Adw.ApplicationWindow
 
     [Gtk.Connect] private readonly Adw.Bin _spinnerContainer;
     [Gtk.Connect] private readonly Gtk.Spinner _spinner;
-    [Gtk.Connect] private readonly Gtk.Box _mainBox;
     [Gtk.Connect] private readonly Adw.HeaderBar _headerBar;
     [Gtk.Connect] private readonly Adw.WindowTitle _title;
     [Gtk.Connect] private readonly Adw.ToastOverlay _toastOverlay;
@@ -180,13 +179,11 @@ public partial class MainWindow : Adw.ApplicationWindow
         _application.AddWindow(this);
         Present();
         _spinnerContainer.SetVisible(true);
-        _mainBox.SetVisible(false);
         _spinner.Start();
         var urlToLaunch = await _controller.StartupAsync();
         _controller.TaskbarItem = await TaskbarItem.ConnectLinuxAsync(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SNAP")) ? $"{_controller.AppInfo.ID}.desktop" : "tube-converter_tube-converter.desktop");
         _spinner.Stop();
         _spinnerContainer.SetVisible(false);
-        _mainBox.SetVisible(true);
         PreventSuspendWhenDownloadingChanged();
         RunInBackgroundChanged();
         if (!string.IsNullOrEmpty(urlToLaunch))
