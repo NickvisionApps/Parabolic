8bbc69626ffe8c3cf33909d0010719f0d6231795
Nick Logozzo <nlogozzo225@gmail.com>
2024-09-11 14:30:30 -0400

    V2024.9.0-beta3

--- libparabolic/src/controllers/mainwindowcontroller.cpp
+++ libparabolic/src/controllers/mainwindowcontroller.cpp
@@ -39,7 +39,7 @@ namespace Nickvision::TubeConverter::Shared::Controllers
         m_keyring{ m_appInfo.getId() },
         m_downloadManager{ m_dataFileManager.get<Configuration>("config").getDownloaderOptions(), m_dataFileManager.get<DownloadHistory>("history"), m_logger }
     {
-        m_appInfo.setVersion({ "2024.9.0-beta2" });
+        m_appInfo.setVersion({ "2024.9.0-beta3" });
         m_appInfo.setShortName(_("Parabolic"));
         m_appInfo.setDescription(_("Download web video and audio"));
         m_appInfo.setChangelog("- Parabolic has been rewritten in C++ for faster performance\n- The length of the kept download history can now be changed in the app's preferences\n- Cookies can now be fetched from a selected browser in Preferences instead of selecting a TXT cookies file (on non-sandbox platforms)\n- Parabolic's Keyring module was rewritten. As a result, all keyrings have been reset and will need to be reconfigured\n- Parabolic will now correctly recognize audio languages with audio description and handle them separately from audio languages without audio description\n- Parabolic will now place playlist downloads into a subdirectory with the playlist's title within the chosen save folder\n- Added the option to allow for immediate download after a URL is validated\n- Fixed validation issues with various sites\n- Fixed an issue where a specified video password was not being used\n- Redesigned user interface\n- Updated yt-dlp");
