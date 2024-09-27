d95007c8a71972c092e40fe85ce5b1e850b173d7
Nick Logozzo <nlogozzo225@gmail.com>
2024-09-27 09:50:21 -0400

    Shared - Fix Progress Crash

--- libparabolic/src/models/download.cpp
+++ libparabolic/src/models/download.cpp
@@ -152,7 +152,7 @@ namespace Nickvision::TubeConverter::Shared::Models
                     m_progressChanged.invoke({ m_id, std::nan(""), 0.0, log });
                     return;
                 }
-                m_progressChanged.invoke({ m_id, (progress[2] != "NA" ? std::stod(progress[2]) : 0.0) / (progress[3] != "NA" ? std::stod(progress[3]) : (progress[4] != "NA" ? std::stod(progress[4]) : 0.0)), (progress[5] != "NA" ? std::stod(progress[5]) : 0.0), log });
+                m_progressChanged.invoke({ m_id, (progress[2] != "NA" && progress[2] != "NA\"" ? std::stod(progress[2]) : 0.0) / (progress[3] != "NA" && progress[3] != "NA\"" ? std::stod(progress[3]) : (progress[4] != "NA" && progress[4] != "NA\"" ? std::stod(progress[4]) : 0.0)), (progress[5] != "NA" && progress[5] != "NA\"" ? std::stod(progress[5]) : 0.0), log });
                 break;
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
