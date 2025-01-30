94fdae35e8fee422e0c7c80f20f5a5ab179f508b
Nick Logozzo <nlogozzo225@gmail.com>
2025-01-30 16:12:00 -0500

    V2025.1.4

--- libparabolic/src/controllers/mainwindowcontroller.cpp
+++ libparabolic/src/controllers/mainwindowcontroller.cpp
@@ -40,7 +40,7 @@ namespace Nickvision::TubeConverter::Shared::Controllers
         m_downloadManager{ m_dataFileManager.get<Configuration>("config").getDownloaderOptions(), m_dataFileManager.get<DownloadHistory>("history"), m_dataFileManager.get<DownloadRecoveryQueue>("recovery"), m_logger },
         m_isWindowActive{ false }
     {
-        m_appInfo.setVersion({ "2025.1.4-next" });
+        m_appInfo.setVersion({ "2025.1.4" });
         m_appInfo.setShortName(_("Parabolic"));
         m_appInfo.setDescription(_("Download web video and audio"));
         m_appInfo.setChangelog("- Added a new Embed Thumbnails option in Preferences to enable/disable Parabolic's downloading of thumbnails separate from metadata\n- Added a disclaimer about embedding thumbnails/subtitles when using generic file types\n- Fixed an issue where the incorrect previous video and/or audio format was selected\n- Fixed an issue where chapters were embedded even if the option was disabled\n- Fixed an issue where splitting media by chapters would result in incorrect media lengths in the split files\n- Fixed an issue where video and audio formats were not selectable on GNOME");
