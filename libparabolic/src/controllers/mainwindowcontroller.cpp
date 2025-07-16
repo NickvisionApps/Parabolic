ff7889d59124f8ffb09cc65c96bf2888d0e10c9b
Nick Logozzo <nlogozzo225@gmail.com>
2025-07-15 23:00:29 -0400

    feat: Version bump

--- libparabolic/src/controllers/mainwindowcontroller.cpp
+++ libparabolic/src/controllers/mainwindowcontroller.cpp
@@ -42,7 +42,7 @@ namespace Nickvision::TubeConverter::Shared::Controllers
         m_downloadManager{ m_dataFileManager.get<Configuration>("config").getDownloaderOptions(), m_dataFileManager.get<DownloadHistory>("history"), m_dataFileManager.get<DownloadRecoveryQueue>("recovery") },
         m_isWindowActive{ false }
     {
-        m_appInfo.setVersion({ "2025.7.0-beta1" });
+        m_appInfo.setVersion({ "2025.7.0-beta2" });
         m_appInfo.setShortName(_("Parabolic"));
         m_appInfo.setDescription(_("Download web video and audio"));
         m_appInfo.setChangelog("- Redesigned the Windows app using WinUI 3\n- Added the ability to remember video and audio formats individually for each file type\n- Fixed an issue where pressing enter in the download dialog would not start the download\n- Fixed an issue where configuration files were not stored properly for the portable Windows build\n- Updated yt-dlp");
