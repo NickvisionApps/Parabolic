6e2344da1509ddfabc1f0f867e014a88f391952f
Nick Logozzo <nlogozzo225@gmail.com>
2025-09-24 19:55:53 -0400

    feat: Version bump

--- libparabolic/src/controllers/mainwindowcontroller.cpp
+++ libparabolic/src/controllers/mainwindowcontroller.cpp
@@ -43,10 +43,10 @@ namespace Nickvision::TubeConverter::Shared::Controllers
 #endif
         m_isWindowActive{ false }
     {
-        m_appInfo.setVersion({ "2025.10.0" });
+        m_appInfo.setVersion({ "2025.10.1-next" });
         m_appInfo.setShortName(_("Parabolic"));
         m_appInfo.setDescription(_("Download web video and audio"));
-        m_appInfo.setChangelog("- Fixed an issue where Parabolic was unable to start\n- Fixed an issue where Parabolic did not notify of new yt-dlp releases\n- Updated GNOME runtime to 49\n- Updated WindowsAppSDK to 1.8\n- Updated yt-dlp");
+        m_appInfo.setChangelog("- ");
         m_appInfo.setSourceRepo("https://github.com/NickvisionApps/Parabolic");
         m_appInfo.setIssueTracker("https://github.com/NickvisionApps/Parabolic/issues/new");
         m_appInfo.setSupportUrl("https://github.com/NickvisionApps/Parabolic/discussions");
