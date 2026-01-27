5ed9e5423bbd0caa8a1025cefc2092611c7d43bb
Nick Logozzo <nlogozzo225@gmail.com>
2026-01-26 19:42:16 -0500

    fix: Build

--- Nickvision.Parabolic.GNOME/Views/AddDownloadDialog.cs
+++ Nickvision.Parabolic.GNOME/Views/AddDownloadDialog.cs
@@ -8,7 +8,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.Media.Playlists;
 
 namespace Nickvision.Parabolic.GNOME.Views;
 
