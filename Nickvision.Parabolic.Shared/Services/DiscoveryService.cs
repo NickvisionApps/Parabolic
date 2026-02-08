2cdfe6751d3da730c60fe61ef4e0b678c43b2937
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-07 21:51:18 -0500

    fix: Entry json parsing

--- Nickvision.Parabolic.Shared/Services/DiscoveryService.cs
+++ Nickvision.Parabolic.Shared/Services/DiscoveryService.cs
@@ -145,35 +145,42 @@ public class DiscoveryService : IDiscoveryService
             throw new Exception(error);
         }
         cancellationToken.ThrowIfCancellationRequested();
+        JsonDocument? json;
         try
         {
-            using var json = JsonDocument.Parse(output);
-            if (json.RootElement.TryGetProperty("entries", out var entriesProperty) && entriesProperty.GetArrayLength() > 0)
-            {
-                var urlInfos = new List<DiscoveryResult>();
-                foreach (var entry in entriesProperty.EnumerateArray())
-                {
-                    cancellationToken.ThrowIfCancellationRequested();
-                    if (entry.TryGetProperty("ie_key", out var ieProperty) && (ieProperty.GetString() ?? string.Empty) == "YoutubeTab" && entry.TryGetProperty("url", out var urlProperty))
-                    {
-                        var urlInfo = await GetForUrlAsync(new Uri(urlProperty.GetString() ?? string.Empty), credential, suggestedSaveFolder, suggestedFilename, cancellationToken);
-                        if (urlInfo is not null)
-                        {
-                            urlInfos.Add(urlInfo);
-                        }
-                    }
-                }
-                if (urlInfos.Count > 0 && json.RootElement.TryGetProperty("title", out var titleProperty))
-                {
-                    return new DiscoveryResult(url, titleProperty.GetString() ?? "Tab", urlInfos);
-                }
-            }
-            return new DiscoveryResult(json.RootElement, _translationService, downloaderOptions, url, suggestedSaveFolder, suggestedFilename);
+            json = JsonDocument.Parse(output);
         }
         catch(JsonException)
         {
             throw new Exception(error);
         }
+        if (json!.RootElement.TryGetProperty("entries", out var entriesProperty) && entriesProperty.GetArrayLength() > 0)
+        {
+            var urlInfos = new List<DiscoveryResult>();
+            foreach (var entry in entriesProperty.EnumerateArray())
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                if(entry.ValueKind != JsonValueKind.Object)
+                {
+                    continue;
+                }
+                if (entry.TryGetProperty("ie_key", out var ieProperty) && (ieProperty.GetString() ?? string.Empty) == "YoutubeTab" && entry.TryGetProperty("url", out var urlProperty))
+                {
+                    var urlInfo = await GetForUrlAsync(new Uri(urlProperty.GetString() ?? string.Empty), credential, suggestedSaveFolder, suggestedFilename, cancellationToken);
+                    if (urlInfo is not null)
+                    {
+                        urlInfos.Add(urlInfo);
+                    }
+                }
+            }
+            if (urlInfos.Count > 0 && json.RootElement.TryGetProperty("title", out var titleProperty))
+            {
+                json.Dispose();
+                return new DiscoveryResult(url, titleProperty.GetString() ?? "Tab", urlInfos);
+            }
+        }
+        json.Dispose();
+        return new DiscoveryResult(json.RootElement, _translationService, downloaderOptions, url, suggestedSaveFolder, suggestedFilename);
     }
 
     private async Task<List<BatchFileEntry>> ParseBatchFileAsync(string batchFilePath, CancellationToken cancellationToken = default)
