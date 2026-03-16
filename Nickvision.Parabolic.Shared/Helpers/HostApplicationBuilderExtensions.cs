006a0a574d81d3fddd0ec04d49c7503ca27b4dcb
Nick Logozzo <nlogozzo225@gmail.com>
2026-03-16 19:41:13 -0400

    fix: Crop thumbnails argument

--- Nickvision.Parabolic.Shared/Helpers/HostApplicationBuilderExtensions.cs
+++ Nickvision.Parabolic.Shared/Helpers/HostApplicationBuilderExtensions.cs
@@ -18,7 +18,7 @@ public static class HostApplicationBuilderExtensions
         {
             var appInfo = new AppInfo("org.nickvision.tubeconverter", "Nickvision Parabolic", "Parabolic")
             {
-                Version = new AppVersion("2026.3.0-beta2"),
+                Version = new AppVersion("2026.3.0-beta3"),
                 Changelog = """
                 - Added macOS app for the GNOME version of Parabolic
                 - Added Windows portable version of Parabolic
--- Nickvision.Parabolic.Shared/Models/Download.cs
+++ Nickvision.Parabolic.Shared/Models/Download.cs
@@ -353,11 +353,22 @@ public partial class Download : IDisposable
             if (downloader.CropAudioThumbnails && Options.FileType.IsAudio)
             {
                 arguments.Add("--exec");
-                arguments.Add($"before_dl:\"{Desktop.System.Environment.FindDependency("ffmpeg") ?? "ffmpeg"}\" -i %(thumbnails.-1.filepath)q -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\" %(thumbnails.-1.filepath)s.tmp.jpg");
-                arguments.Add("--exec");
-                arguments.Add("before_dl:rm %(thumbnails.-1.filepath)q");
-                arguments.Add("--exec");
-                arguments.Add("before_dl:mv %(thumbnails.-1.filepath)s.tmp.jpg %(thumbnails.-1.filepath)q");
+                arguments.Add($"before_dl:\"{Desktop.System.Environment.FindDependency("ffmpeg") ?? "ffmpeg"}\" -i %(thumbnails.-1.filepath)q -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\" \"%(thumbnails.-1.filepath)s.tmp.jpg\"");
+                if(OperatingSystem.IsWindows())
+                {
+                    arguments.Add("--exec");
+                    arguments.Add("before_dl:del %(thumbnails.-1.filepath)q");
+                    arguments.Add("--exec");
+                    arguments.Add("before_dl:move \"%(thumbnails.-1.filepath)s.tmp.jpg\" %(thumbnails.-1.filepath)q");
+                }
+                else
+                {
+                    arguments.Add("--exec");
+                    arguments.Add("before_dl:rm %(thumbnails.-1.filepath)q");
+                    arguments.Add("--exec");
+                    arguments.Add("before_dl:mv \"%(thumbnails.-1.filepath)s.tmp.jpg\" %(thumbnails.-1.filepath)q");
+                }
+                
             }
         }
         if (downloader.EmbedChapters)
