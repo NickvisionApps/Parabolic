487a09773a6ce85c6abc6ad8d8d7bc426f39a8c0
Nick Logozzo <nlogozzo225@gmail.com>
2026-03-22 09:42:52 -0400

    fix: Observable collection

--- Nickvision.Parabolic.WinUI/Helpers/SelectionItemListExtensions.cs
+++ Nickvision.Parabolic.WinUI/Helpers/SelectionItemListExtensions.cs
@@ -3,6 +3,7 @@ using Nickvision.Desktop.Keyring;
 using Nickvision.Parabolic.Shared.Models;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 
 namespace Nickvision.Parabolic.WinUI.Helpers;
@@ -17,6 +18,51 @@ public static class SelectionItemListExtensions
     extension(IEnumerable<SelectionItem<Credential>> items)
     {
         public ObservableCollection<BindableCredentialSelectionItem> ToBindableCredentialSelectionItems() => new ObservableCollection<BindableCredentialSelectionItem>(items.Select(i => new BindableCredentialSelectionItem(i)));
+    }
+
+    extension(ObservableCollection<SelectionItem<Credential>> items)
+    {
+        public ObservableCollection<BindableCredentialSelectionItem> ToBindableCredentialSelectionItems()
+        {
+            var collection = new ObservableCollection<BindableCredentialSelectionItem>(items.Select(i => new BindableCredentialSelectionItem(i)));
+            items.CollectionChanged += (_, e) =>
+            {
+                if (collection is null)
+                {
+                    return;
+                }
+                switch (e.Action)
+                {
+                    case NotifyCollectionChangedAction.Add when e.NewItems is not null:
+                        foreach (SelectionItem<Credential> item in e.NewItems)
+                        {
+                            collection.Add(new BindableCredentialSelectionItem(item));
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Remove when e.OldItems is not null:
+                        foreach (SelectionItem<Credential> item in e.OldItems)
+                        {
+                            var bindable = collection.FirstOrDefault(b => b.Value == item.Value);
+                            if (bindable is not null)
+                            {
+                                collection.Remove(bindable);
+                            }
+                        }
+                        break;
+                    case NotifyCollectionChangedAction.Replace when e.NewItems is not null && e.NewStartingIndex >= 0 && e.NewStartingIndex < collection.Count:
+                        collection[e.NewStartingIndex] = new BindableCredentialSelectionItem((SelectionItem<Credential>)e.NewItems[0]!);
+                        break;
+                    default:
+                        collection.Clear();
+                        foreach (var item in items)
+                        {
+                            collection.Add(new BindableCredentialSelectionItem(item));
+                        }
+                        break;
+                }
+            };
+            return collection;
+        }
     }
 
     extension(IEnumerable<SelectionItem<HistoricDownload>> items)
