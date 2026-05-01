24dd43755e7f10224e4d439809f4509b874c667f
Nick Logozzo <nlogozzo225@gmail.com>
2026-04-30 22:07:24 -0400

    fix: X quoted downloads

--- Nickvision.Parabolic.Shared/Helpers/HostApplicationBuilderExtensions.cs
+++ Nickvision.Parabolic.Shared/Helpers/HostApplicationBuilderExtensions.cs
@@ -25,6 +25,7 @@ public static class HostApplicationBuilderExtensions
                 - Fixed an issue where portrait/vertical videos in playlists downloaded at incorrect resolutions
                 - Fixed an issue where downloads from sites with muxed-only streams would fail
                 - Fixed an issue where downloading a time frame clip from a long video produced an incomplete result
+                - Fixed an issue where X/Twitter quoted downloads could produce the same video twice
                 - Fixed an issue where deno was unable to be updated in-app on Linux
                 - Fixed an issue where Parabolic would not start on KDE desktops
                 - Fixed an issue where Parabolic did not open links from browser extension on Windows
